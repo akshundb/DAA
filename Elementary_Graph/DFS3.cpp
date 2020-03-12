793945fda0b4db8fcc8e0da856694b93fcacbc86
Akshun <akshundb@gmail.com>
2020-03-12 16:46:35 +0530

    DFS3 added 1/0 return

---
+++ Elementary_Graph/DFS3.cpp
@@ -0,0 +1,33 @@
+#include "graph.h"
+#include<iostream>
+
+#define MAX 10000
+
+using namespace std;
+
+bool visited[MAX];
+
+int DFS(struct Graph G, int s, int t)
+{
+
+        struct node *temp = G.N[s];
+
+        visited[s] = 1;
+
+        if(s == t)
+        {
+          return 1;
+        }
+
+
+        while(temp!=NULL)
+        {
+            int connectedVertex = temp->data;
+            if(visited[connectedVertex] == 0)
+            {
+                return DFS(G, connectedVertex, t);
+            }
+
+            temp = temp->next;
+        }
+}
