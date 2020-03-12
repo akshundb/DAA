6bf7dd1be56242ed274962621299dd9fc270e247
vinayak-sethi <sethivinayak14600@gmail.com>
2020-03-12 16:57:34 +0530

    updated DFS2.cpp

--- Elementary_Graph/DFS2.cpp
+++ Elementary_Graph/DFS2.cpp
@@ -24,28 +24,3 @@ void DFS(struct Graph G,int s)
 	temp = temp->next;
 	}
 }
-
-int main()
-{
-	Graph g;
-	
-	g.n = 7;
-	
-	g.insertEdge(0,1, 2);
-	g.insertEdge(2,1, 21);
-	g.insertEdge(1,3, 20);
-	g.insertEdge(4,2, 88);
-	g.insertEdge(5,6, 10);
-	g.insertEdge(0,6, 1);
-	
-	cout << endl;
-	
-	cout << "Linking of nodes is as follows " << endl;
-	g.print();
-	cout << "\n";
-
-	DFS(g,4);
-	cout << endl;
-		
-	return 0;
-}
