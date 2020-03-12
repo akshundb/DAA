d45a358ef7f84af74f6f1f3cd4242124f35c8064
Akshun <akshundb@gmail.com>
2020-03-12 14:54:24 +0530

    jdkj

--- Elementary_Graph/graph.h
+++ Elementary_Graph/graph.h
@@ -4,3 +4,14 @@ void BFS(Graph, int s, int t);
 void BFS_SPATH(Graph G, int s, int flag);
 void LPATH(Graph G, int flag);
 
+void MAX_Weight_MST(Graph G);
+
+void TEST_bipartite(Graph G);
+void Test_articulationpoints(Graph G);
+void Test_bridges(Graph G);
+
+void MST_Prim(Graph G);
+void MST_Kruskal(Graph G);
+void MST_youralgo(Graph G);
+
+
