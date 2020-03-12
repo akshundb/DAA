00bbedb752e6b3861dfc4e500084a1e4e9d0f56e
Sai Kaushik S <saikaushik609@gmail.com>
2020-03-12 15:19:44 +0530

    BFS(graph G, int s)

---
+++ Elementary_Graph/BFS2.cpp
@@ -0,0 +1,52 @@
+include "graph.h"
+
+struct node* head = front = rear = NULL;
+
+void enqueue(int x){
+	struct node* temp = (struct node*) malloc (sizeof(struct node));
+	temp->data = x;
+	if(head == NULL){
+		front = temp;
+		rear = temp;
+		temp->next = temp;
+		head = temp;
+	}
+	else{
+		rear->next = temp;
+		rear = temp;
+		temp->next = head;
+	}
+}
+
+void dequeue(){
+	front = front->next;
+	head = head->next;
+	rear->next = head;
+}
+
+int empty(){
+    if(front == rear)
+        return 1;
+    return 0;
+}
+
+void BFS(graph G, int s){
+    bool visit[n];
+    for(int i = 0; i < n; i++)
+        visit[i] = false;
+    visit[s] = true;
+    enqueue(s);
+    while(!empty()){
+        cout<<pop()<<" ";
+        struct node* temp = N[s];
+        int i;
+        while(temp){
+            i = temp->data;
+            if(!visit[i]){
+                visit[i] = true;
+                push(i);
+                temp = temp->next;
+            }
+        }
+    }
+}
