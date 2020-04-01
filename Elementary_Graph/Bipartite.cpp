05f1b3e037f0c05aff4b9ea8fd5587818e9dd628
Sai Kaushik S <saikaushik609@gmail.com>
2020-04-01 22:10:23 +0530

    Update Bipartite.cpp

--- Elementary_Graph/Bipartite.cpp
+++ Elementary_Graph/Bipartite.cpp
@@ -1,11 +1,11 @@
 #include"graph.h"
 
-bool isBipartite(Graph G, int s){
-    int colorArr[G.n];
+bool bipartite(Graph G, int s){
+    int color[G.n];
     for (int i = 0; i < G.n; ++i)
-        colorArr[i] = -1;
+        color[i] = -1;
     
-    colorArr[s] = 1;
+    color[s] = 1;
     queue <int> q;
     q.push(s);
     
@@ -15,11 +15,11 @@ bool isBipartite(Graph G, int s){
 		struct node* temp = G.N[u];
 		while(temp!=NULL){
 			int v = temp->data;
-			if(colorArr[v] == -1){
-				colorArr[v] = 1 - colorArr[u];
+			if(color[v] == -1){
+				color[v] = 1 - color[u];
                 q.push(v);
 			}
-			else if(colorArr[v] == colorArr[u])
+			else if(color[v] == color[u])
 				return false;
 			temp = temp->next;
 		}
